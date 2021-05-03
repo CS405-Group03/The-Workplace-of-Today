035b8212ad2879765b27196f9c289eb783ded7d9
Crista Mae <82898714+cristachiangco@users.noreply.github.com>
2021-05-03 11:59:56 +0800

    meow

---
+++ resources/views/layouts/bacci_signup.blade.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <div id="signup_content">
+            <div id="active_acc">
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </body>
+</html>
