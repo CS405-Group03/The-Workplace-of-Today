184d948524d15f466ec8af70e6703ad2f41cba4a
Laurence Angelo <82821861+BatuLaurenceAngelo@users.noreply.github.com>
2021-06-10 12:32:38 +0800

    Anchor Navigation to Another Page

--- public/js/non-empty.js
+++ public/js/non-empty.js
@@ -1,14 +1,12 @@
-function required()
-{
-var empt = document.forms["form1"]["form_gender"].value;
-if (empt == "")
-{
-alert("Please input a Value");
-return false;
-}
-else 
-{
-alert('Code has accepted : you can try another');
-return true; 
-}
+function required(){
+    var empt = document.forms["form1"]["form_gender"].value;
+    
+    if (empt == ""){
+        alert("Please input a Value");
+        return false;
+    }
+    else {
+        alert('Code has accepted : you can try another');
+        return true; 
+    }
 }
